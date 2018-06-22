0f54b1dbbd6c200ea86fbd944d51752edad08a77
RuthSalvador <ruth.abby.sz@gmail.com>
2018-06-22 09:25:46 -0500

    añade estilos para presentación chapter

--- src/js/pages/Home.js
+++ src/js/pages/Home.js
@@ -4,6 +4,7 @@ const figureUser = document.createElement('figure');
 const postContain = document.createElement('form');
 const postInput = document.createElement('input');
 const postBtn = document.createElement('button');
+const allPost = document.createElement('div');
 const containerPosts = document.createElement('div');
 const postType = document.createElement('select');
 const optionPublic = document.createElement('option');
@@ -17,12 +18,17 @@ optionFriends.value = 'friends';
 root.appendChild(postOfUser);
 root.appendChild(containerPosts);
 postOfUser.appendChild(figureUser);
-postOfUser.appendChild(postContain);
+postOfUser.appendChild(allPost);
+
+allPost.appendChild(postContain);
+allPost.appendChild(document.createElement('h2')).appendChild(document.createTextNode('Mis post'));
 postContain.appendChild(postInput);
 postContain.appendChild(postType);
 postContain.appendChild(addBtn(postBtn, 'Publicar'));
 postType.appendChild(optionPublic).appendChild(document.createTextNode('Público'));
 postType.appendChild(optionFriends).appendChild(document.createTextNode('Amigos'));
+
+figureUser.appendChild(document.createTextNode('acá va a ir el usuario'));
 
 postBtn.addEventListener('click', () => {
   createPost(postInput, containerPosts, postBtn);
